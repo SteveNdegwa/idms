fedd472939be5599dab81ab7d569504e52c735b7
SteveNdegwa <stevencallistus19@gmail.com>
2024-11-05 07:59:42 +0300

    feat: add API communication authorization

--- api/backend/middlewares.py
+++ api/backend/middlewares.py
@@ -1,6 +1,28 @@
+import logging
+import base64
+
+from django.http import JsonResponse
+
+from api.backend.services import APIUserService
+
+lgr = logging.getLogger(__name__)
+
 def AuthorizeMiddleware(get_response):
-    def middleware(request):
-        if str(request.get_full_path()).startswith("/api"):
-            print("API request")
-        return get_response(request)
-    return middleware
+    try:
+        def middleware(request):
+            url_whitelist = []
+            if str(request.get_full_path()).startswith("/api") and request.get_full_path() not in url_whitelist:
+                auth_token = request.headers.get("Authorization", "")
+                if auth_token:
+                    auth_token = auth_token.split()
+                    if auth_token[0] == "Basic":
+                        username, password = (base64.b64decode(auth_token[1])).decode("utf-8").split(':')
+                        if APIUserService().get(username=username, password=password):
+                            return get_response(request)
+                return JsonResponse({"code": "888.888.888", "message": "Not Authorized"})
+            else:
+                return get_response(request)
+        return middleware
+    except Exception as ex:
+        lgr.exception("AuthorizeMiddleware exception: %s" % ex)
+        return JsonResponse({"code": "888.888.888", "message": "Not Authorized"})
