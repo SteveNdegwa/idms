f2b674c05472002c51839999a8e40b4921acbc4a
SteveNdegwa <stevencallistus19@gmail.com>
2024-11-05 08:03:17 +0300

    feat: add API communication authorization

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path, include
 
+from api.views import APIManager
+
 urlpatterns = [
+    re_path(r'^create-api-user/', APIManager().create_api_user),
+    re_path(r'^refresh-api-keys/', APIManager().refresh_api_keys),
     re_path(r'^users/', include('users.urls')),
     re_path(r'^organisations/', include('organisations.urls')),
     re_path(r'^systems/', include('systems.urls')),
